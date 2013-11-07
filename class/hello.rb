29c39966f9453d7ad84e380d47998917d619e00b
takasing <takaty2011@gmail.com>
2013-11-07 13:11:42 +0900

    mod

--- class/hello.rb
+++ class/hello.rb
@@ -3,10 +3,6 @@ class Hello
   attr_accessor :name
   @@count = 0
 
-  def initialize(name = "default")
-    @name = name
-  end
-
   def greet
     puts "Hello, #{@name}"
     @@count += 1
@@ -14,10 +10,6 @@ class Hello
 
   def self.count
     @@count
-  end
-
-  def self.double
-    @@count * 2
   end
 
 end
