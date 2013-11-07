f40509e691e2e341ffd58cfac802771a42c8d37d
takasing <takaty2011@gmail.com>
2013-11-07 09:28:54 +0900

    add output image.

--- block/with_param.rb
+++ block/with_param.rb
@@ -3,15 +3,18 @@ require './block'
 include Block
 
 # no block no param
-double_param
+# => Hello, hoge
+double_param 
 
 # no param
+# => Hello! hoge!!
 double_param do |greet, name|
   puts "#{greet}! #{name}!!"
 end
 
 # param
+# => Bonjour! Bonjour!! takasing!!!
 double_param "Bonjour", "takasing" do |greet, name|
-  puts "#{greet}! #{greet}!! #{name}"
+  puts "#{greet}! #{greet}!! #{name}!!!"
 end
 
