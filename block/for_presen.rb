5739c3c94aa6815dfe309810bfa38ebd22453991
takasing <takaty2011@gmail.com>
2013-11-07 09:34:00 +0900

    remove module.

--- block/for_presen.rb
+++ block/for_presen.rb
@@ -1,15 +1,13 @@
-module ModuleName
-  def method(param1, param2)
-    if block_given?
-      # ブロックありの挙動
-      yield
-    else
-      # ブロックなしの挙動
-    end
+#!/usr/bin/env ruby
+def method(param1, param2)
+  if block_given?
+    # ブロックありの挙動
+    yield
+  else
+    # ブロックなしの挙動
   end
 end
 
-include ModuleName
 method "param1", "param2" do |param1, param2|
   # 与えたい挙動
 end
